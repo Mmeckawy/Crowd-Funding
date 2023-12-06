fa9e92683116f5fe8604153ccb8c5346191ba665
Mmeckawy <dmeckawy@gmail.com>
2023-12-06 03:48:27 +0200

    All buttons working properly except for delete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox,ttk
 import authentication_mod as auth
 import project_mod as proj
 
@@ -20,38 +20,138 @@ def register():
 
 def projects():
    project_gui = tk.Toplevel()
-   project_gui.title("Log in")
+   project_gui.title("Project")
    project_gui.geometry("500x400")
    project_gui['background']='#b5a8fb'
    
-   title_label = tk.Label(project_gui, text = "Title", background='#b5a8fb').place(x = 150,y = 30)
+   title_label = tk.Label(project_gui, text = "Title", background='#b5a8fb').place(x = 50,y = 30)
    title_entry = tk.Entry(project_gui)
-   title_entry.place(x = 280, y = 30)
+   title_entry.place(x = 180, y = 30)
 
-   details_label = tk.Label(project_gui, text = "Details", background='#b5a8fb').place(x = 150,y = 60)
+   details_label = tk.Label(project_gui, text = "Details", background='#b5a8fb').place(x = 50,y = 60)
    details_entry = tk.Entry(project_gui)
-   details_entry.place(x = 280, y = 60)
+   details_entry.place(x = 180, y = 60)
 
-   target_label = tk.Label(project_gui, text = "Target", background='#b5a8fb').place(x = 150,y = 90)
+   target_label = tk.Label(project_gui, text = "Target", background='#b5a8fb').place(x = 50,y = 90)
    target_entry = tk.Entry(project_gui)
-   target_entry.place(x = 280, y = 90)
+   target_entry.place(x = 180, y = 90)
 
-   start_date_label = tk.Label(project_gui, text = "Start Date", background='#b5a8fb').place(x = 150,y = 120)
+   start_date_label = tk.Label(project_gui, text = "Start Date", background='#b5a8fb').place(x = 50,y = 120)
    start_date_entry = tk.Entry(project_gui)
-   start_date_entry.place(x = 280, y = 120)
+   start_date_entry.place(x = 180, y = 120)
 
-   end_date_label = tk.Label(project_gui, text = "End Date", background='#b5a8fb').place(x = 150,y = 150)
+   end_date_label = tk.Label(project_gui, text = "End Date", background='#b5a8fb').place(x = 50,y = 150)
    end_date_entry = tk.Entry(project_gui)
-   end_date_entry.place(x = 280, y = 150)
+   end_date_entry.place(x = 180, y = 150)
 
-   title = title_entry.get()
-   details = details_entry.get()
-   target = target_entry.get()
-   start_date = start_date_entry.get()
-   end_date = end_date_entry.get()
+   project_listbox = tk.Listbox(project_gui)
+   project_listbox.place(x = 400, y = 400)
 
-   result = proj.new_project(title, details, target, start_date, end_date)
-   messagebox.showinfo("Result", result)
+    # Create a Treeview widget
+   tree = ttk.Treeview(project_gui)
+
+   # Define the columns
+   tree["columns"]=("one","two","three","four","five")
+   tree.column("#0", width=100, minwidth=25)
+   tree.column("one", width=100, minwidth=25)
+   tree.column("two", width=100, minwidth=25)
+   tree.column("three", width=100, minwidth=25)
+   tree.column("four", width=100, minwidth=25)
+   tree.column("five", width=100, minwidth=25)
+
+   # Define the headings
+   tree.heading("#0",text="Project ID",anchor=tk.W)
+   tree.heading("one", text="Title",anchor=tk.W)
+   tree.heading("two", text="Details",anchor=tk.W)
+   tree.heading("three", text="Target",anchor=tk.W)
+   tree.heading("four", text="start_date",anchor=tk.W)
+   tree.heading("five", text="end_date",anchor=tk.W)
+
+   for project_id in proj.projects:
+      project_listbox.insert(tk.END, project_id)
+
+   def create():
+      title = title_entry.get()
+      details = details_entry.get()
+      target = target_entry.get()
+      start_date = start_date_entry.get()
+      end_date = end_date_entry.get()
+
+      result = proj.new_project(title, details, target, start_date, end_date)
+      messagebox.showinfo("Result", result)
+
+   def view():
+      tree.place(x = 320, y = 50)
+      # Insert the data
+      for project in proj.projects:
+         tree.insert("", tk.END, text=project, values=(proj.projects[project]['title'],proj.projects[project]['details'], 
+                           proj.projects[project]['target'], proj.projects[project]['start_date'],proj.projects[project]['end_date']))
+         print(proj.projects)
+         
+   def update():
+       # Get the selected item
+      selected_item = tree.selection()[0]
+
+      # Get the project details from the selected item
+      project_details = tree.item(selected_item, 'values')
+
+      # Get the project ID from the project details
+      project_id = project_details[0]
+
+      # Get the new project details
+      title = title_entry.get()
+      details = details_entry.get()
+      target = target_entry.get()
+      start_date = start_date_entry.get()
+      end_date = end_date_entry.get()
+
+      # Update the project details in the Treeview
+      tree.item(selected_item, values=(title, details, target, start_date, end_date))
+
+      # Update the project in the proj.projects dictionary
+      proj.projects[project_id].update({'title': title, 'details': details, 'target': target, 'start_date': start_date, 'end_date': end_date})
+      print(proj.projects)
+         
+
+
+   def delete():
+      if tree.selection():
+         # Get the selected item
+         selected_item = tree.selection()[0]
+
+         # Delete the selected item
+         tree.delete(selected_item)
+
+         # Get the project details from the selected item
+         project_details = tree.item(selected_item, 'values')
+
+         # Get the project title from the project details
+         project_title = project_details[0]
+
+         # Delete the project
+         del proj.projects[project_title]
+
+         # Clear the Treeview
+         for item in tree.get_children():
+            tree.delete(item)
+
+         # Repopulate the Treeview
+         for project in proj.projects:
+            tree.insert("", tk.END, text=project, values=(proj.projects[project]['title'],proj.projects[project]['details'], 
+                           proj.projects[project]['target'], proj.projects[project]['start_date'],proj.projects[project]['end_date'])) 
+
+   create_button = tk.Button(project_gui, text="Create", command=create)
+   create_button.place(x = 70, y = 250)
+
+   update_button = tk.Button(project_gui, text="Update", command=update)
+   update_button.place(x = 150, y = 250)
+
+   view_button = tk.Button(project_gui, text="View", command=view)
+   view_button.place(x = 220, y = 250)
+
+   delete_button = tk.Button(project_gui, text="Delete", command=delete)
+   delete_button.place(x = 270, y = 250)
+
    project_gui.mainloop()
 
 def login():
@@ -73,6 +173,7 @@ def login():
 
       if auth.authenticate_user(email2, password_login):
         messagebox.showinfo("Log in", "Logged in successfully")
+        projects()
       else:
        messagebox.showerror("Log in", "Failed to login")
 
--- project_mod.py
+++ project_mod.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 
 projects = {}
 
@@ -9,25 +10,21 @@ def validate_target(target="target"):
         return False
     
 def validate_date(start_date = None, end_date = None):
-    pattern = r'\d{2}-\d{2}-\d{4}'
+   pattern = r'\d{2}-\d{2}-\d{4}'
 
-    if not re.match(pattern, start_date) or not re.match(pattern, end_date):
-        return "Invalid Date"
-    else:
-        if start_date[6:9] > end_date[6:9]:
-            return "Invalid End Date"
-        elif start_date[6:9] == end_date[6:9]:
-            if start_date[3:4] > end_date[3:4]:
-                return "Invalid End Date"
-            elif start_date[3:4] == end_date[3:4]:
-                if start_date[1:2] > end_date[1:2]:
-                    return "Invalid End Date"
-                else:
-                    return "Valid"
-            else:
-                return "Valid"
-        else:
-            return "Valid"
+   if not re.match(pattern, start_date) or not re.match(pattern, end_date):
+       return "Invalid Date"
+   else:
+       try:
+           start_date = datetime.strptime(start_date, '%d-%m-%Y')
+           end_date = datetime.strptime(end_date, '%d-%m-%Y')
+       except ValueError:
+           return "Invalid Date"
+
+       if start_date > end_date:
+           return "Invalid End Date"
+       else:
+           return "Valid"
 
 def new_project(title, details, target, start_date, end_date):
     if validate_target(target) == True:
@@ -35,10 +32,9 @@ def new_project(title, details, target, start_date, end_date):
     else:
         return "Enter digits only"
     
-    if validate_date(start_date,end_date) == "Valid":
-        pass
-    else:
+    if not validate_date(start_date,end_date) == "Valid":
         return "Enter Date Again"
-    
-    projects[title] = {'title' : title, 'details': details, 'target' : target, 'start_date' : start_date, 'end_date' : end_date}
-    print(projects)
+    else:
+        projects[title] = {'title' : title, 'details': details, 'target' : target, 'start_date' : start_date, 'end_date' : end_date}
+        print(projects)
+        return "Project Created successfully"
